7399d30f45c496dddfc97064d201290c77d65bf2
Your Name <you@example.com>
2023-07-16 16:45:47 -0300

    hero file updated with two real info

--- src/tests/hero.js
+++ src/tests/hero.js
@@ -15,32 +15,32 @@ const get_attributes = (str, agi, int, cha) => {
     }
 }
 
-const get_skills = (s) => {
+const get_skills = (skills) => {
     return {
-        ironflesh: s.ironflesh | 0,
-        power_strike: s.power_strike | 0,
-        power_throw: s.power_throw | 0,
-        power_draw: s.power_draw | 0,
-        weapon_master: s.weapon_master | 0,
-        shield: s.shield | 0,
-        athletics: s.athletics | 0,
-        riding: s.riding | 0,
-        horse_archery: s.horse_archery | 0,
-        looting: s.looting | 0,
-        trainer: s.trainer | 0,
-        tracking: s.tracking | 0,
-        tactics: s.tactics | 0,
-        path_finding: s.path_finding | 0,
-        spotting: s.spotting | 0,
-        inventory_management: s.inventory_management | 0,
-        wound_treatment: s.wound_treatment | 0,
-        surgery: s.surgery | 0,
-        first_aid: s.first_aid | 0,
-        engineer: s.engineer | 0,
-        persuasion: s.persuasion | 0,
-        prisoner_management: s.prisoner_management | 0,
-        leadership: s.leadership | 0,
-        trade: s.trade | 0
+        ironflesh: skills.ironflesh | 0,
+        power_strike: skills.power_strike | 0,
+        power_throw: skills.power_throw | 0,
+        power_draw: skills.power_draw | 0,
+        weapon_master: skills.weapon_master | 0,
+        shield: skills.shield | 0,
+        athletics: skills.athletics | 0,
+        riding: skills.riding | 0,
+        horse_archery: skills.horse_archery | 0,
+        looting: skills.looting | 0,
+        trainer: skills.trainer | 0,
+        tracking: skills.tracking | 0,
+        tactics: skills.tactics | 0,
+        path_finding: skills.path_finding | 0,
+        spotting: skills.spotting | 0,
+        inventory_management: skills.inventory_management | 0,
+        wound_treatment: skills.wound_treatment | 0,
+        surgery: skills.surgery | 0,
+        first_aid: skills.first_aid | 0,
+        engineer: skills.engineer | 0,
+        persuasion: skills.persuasion | 0,
+        prisoner_management: skills.prisoner_management | 0,
+        leadership: skills.leadership | 0,
+        trade: skills.trade | 0
     }
 }
 
@@ -52,8 +52,10 @@ const hero = (info, attributes, skills) => {
     }
 }
 
-const ymira = hero(["Ymira", 1], [7, 7, 8, 9], {})
-const marnid = hero(["Marnid", 2, 46], [10, 11, 10, 6], { ironflesh: 3 })
+// ymira health = 41
+const ymira = hero(["Ymira", 1], [6, 9, 11, 6], { athletics: 1, riding: 3, inventory_management: 3, wound_treatment: 1, surgery: 1, first_aid: 3, trade: 3 })
+// deshavi health = 45
+const deshavi = hero(["Deshavi", 2], [8, 9, 10, 6], { ironflesh: 1, power_draw: 2, weapon_master: 1, athletics: 2, tracking: 2, path_finding: 3, spotting: 3, inventory_management: 2 })
 
 console.log(ymira)
-console.log(marnid)
+console.log(deshavi)
